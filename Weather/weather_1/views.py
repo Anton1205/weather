0fb4c2b594a95ccab831b1a53f3032f29296fecc
Anton <antonchik18@icloud.com>
2022-11-17 16:17:34 +0300

    files

--- Weather/weather_1/views.py
+++ Weather/weather_1/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
-
+import requests
 
 def index(request):
+    appid = 'd6ed9b41d7205a064759ad98176b28ef'
+    url = "https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=" + appid
+
+    city = "Madrid"
+    res = requests.get(url.format(city))
+
+    print(res.text)
     return render(request, 'weather_1/index.html')
